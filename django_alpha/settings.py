8be839b8115f1ade79cdb90792e8e51d82373e3e
furkan <furkan@furkan-MacBook-Pro.local>
2022-04-21 18:06:09 +0300

    idk

--- django_alpha/settings.py
+++ django_alpha/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'django_alpha.wsgi.application'
 
 
 # Data e
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases 
 
 DATABASES = {
     'default': {
